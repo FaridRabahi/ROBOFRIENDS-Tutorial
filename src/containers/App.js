c97c64c42bc31a003ab6f6f1707224472da5fc46
Farid <118@cegepoutaouais.qc.ca>
2022-11-18 23:21:39 -0500

    Initialize project using Create React App

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+function App() {
+  //constructor() {
+  // super();
+  // this.state = {
+  //   robots: [],
+  //   searchfield: "",
+  //};
+  // }
+
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  //componentDidMount() {
+  //  fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then((response) => response.json())
+  //   .then((users) => this.setState({ robots: users }));
+  // }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  }, []); /*Empty array as second argumemts so useEffect wont render again and enter a loop*/
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  return !robots.length ? (
+    <h1> Loading </h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RobotFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
+}
+
+export default App;
